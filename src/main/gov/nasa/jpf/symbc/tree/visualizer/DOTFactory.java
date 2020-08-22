4a6d89006674424df1a56ccbcc974d90d0dd8238
Francis Nixon <13nixonf@gmail.com>
2020-08-21 18:08:33 -0700

    add variable information in dot exporter.

--- src/main/gov/nasa/jpf/symbc/tree/visualizer/DOTFactory.java
+++ src/main/gov/nasa/jpf/symbc/tree/visualizer/DOTFactory.java
@@ -3,16 +3,16 @@
  * Administrator of the National Aeronautics and Space Administration.
  * All rights reserved.
  *
- * Symbolic Pathfinder (jpf-symbc) is licensed under the Apache License, 
+ * Symbolic Pathfinder (jpf-symbc) is licensed under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
- *        http://www.apache.org/licenses/LICENSE-2.0. 
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package gov.nasa.jpf.symbc.tree.visualizer;
@@ -30,6 +30,8 @@ import gov.nasa.jpf.jvm.bytecode.JVMInvokeInstruction;
 import gov.nasa.jpf.symbc.numeric.PathCondition;
 import gov.nasa.jpf.symbc.tree.NodeFactory;
 import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.LocalVarInfo;
+import gov.nasa.jpf.vm.StackFrame;
 import gov.nasa.jpf.vm.VM;
 import gov.nasa.jpf.vm.bytecode.InvokeInstruction;
 import gov.nasa.jpf.vm.bytecode.ReturnInstruction;
@@ -54,11 +56,11 @@ public class DOTFactory extends NodeFactory<Node> {
     if(instr instanceof InvokeInstruction)
       attrs.addAll(this.getNodeAttr((InvokeInstruction)instr, pc));
     else if(instr instanceof ReturnInstruction)
-      attrs.addAll(this.getNodeAttr((ReturnInstruction)instr, pc));
+      attrs.addAll(this.getNodeAttr((ReturnInstruction)instr, pc, vm));
     else if(instr instanceof IfInstruction)
-      attrs.addAll(this.getNodeAttr((IfInstruction)instr, pc));
+      attrs.addAll(this.getNodeAttr((IfInstruction)instr, pc,vm));
     else
-      attrs.addAll(this.getNodeAttr(instr, pc));
+      attrs.addAll(this.getNodeAttr(instr, pc, vm));
 
     for(Attribute attr : attrs)
       targetNode.setAttribute(attr);
@@ -83,13 +85,13 @@ public class DOTFactory extends NodeFactory<Node> {
     return attrs;
   }
 
-  protected List<Attribute> getNodeAttr(ReturnInstruction instr, PathCondition pc) {
+  protected List<Attribute> getNodeAttr(ReturnInstruction instr, PathCondition pc, VM vm) {
     List<Attribute> attrs = new LinkedList<Attribute>();
 
     StringBuilder lblBuilder = new StringBuilder();
     lblBuilder.append(instr.getMnemonic()).append("\\n");
     lblBuilder.append("(").append(instr.getFilePos()).append(")\\n");
-    lblBuilder.append(getPathConditionString(pc));
+    lblBuilder.append(getPathConditionString(pc,vm));
     Instruction next = instr.getNext();
     if(next != null)
       lblBuilder.append("Returning to:\\n").append(next.getMethodInfo().getFullName());
@@ -99,12 +101,12 @@ public class DOTFactory extends NodeFactory<Node> {
     return attrs;
   }
 
-  protected List<Attribute> getNodeAttr(IfInstruction instr, PathCondition pc) {
+  protected List<Attribute> getNodeAttr(IfInstruction instr, PathCondition pc, VM vm) {
     List<Attribute> attrs = new LinkedList<Attribute>();
     StringBuilder lblBuilder = new StringBuilder();
     lblBuilder.append(instr.getMnemonic()).append("\\n");
     lblBuilder.append("(").append(instr.getFilePos()).append(")\\n");
-    lblBuilder.append(getPathConditionString(pc));
+    lblBuilder.append(getPathConditionString(pc,vm));
 
     attrs.add(new Attribute(Attribute.NODE, Attribute.LABEL_ATTR, lblBuilder.toString()));
     attrs.add(new Attribute(Attribute.NODE, Attribute.COLOR_ATTR, Color.blue));
@@ -112,29 +114,49 @@ public class DOTFactory extends NodeFactory<Node> {
     return attrs;
   }
 
-  protected List<Attribute> getNodeAttr(Instruction instr, PathCondition pc) {
+  protected List<Attribute> getNodeAttr(Instruction instr, PathCondition pc, VM vm) {
 
     List<Attribute> attrs = new LinkedList<Attribute>();
     StringBuilder lblBuilder = new StringBuilder();
     lblBuilder.append(instr.getMnemonic()).append("\\n");
     lblBuilder.append("(").append(instr.getFilePos()).append(")\\n");
-    lblBuilder.append(getPathConditionString(pc));
+    lblBuilder.append(getPathConditionString(pc,vm));
     attrs.add(new Attribute(Attribute.NODE, Attribute.LABEL_ATTR, lblBuilder.toString()));
     attrs.add(new Attribute(Attribute.NODE, Attribute.COLOR_ATTR, Color.black));
     return attrs;
   }
 
-  protected String getPathConditionString(PathCondition pc) {
-    if(pc != null) {
-      StringBuilder pcBuilder = new StringBuilder();
-      String[] pcs = pc.header.stringPC().split("&&");  
-      for(int i = 0; i < pcs.length; i++) {
+  protected String getPathConditionString(PathCondition pc, VM vm) {
+    final StackFrame topFrame = vm.getCurrentThread().getModifiableTopFrame();
+    final StringBuilder pcBuilder = new StringBuilder();
+    LocalVarInfo[] localVars = topFrame.getLocalVars();
+    if (localVars != null) {
+      int before = pcBuilder.length();
+      for (int i = 0; i < localVars.length; i++) {
+        LocalVarInfo localVar = localVars[i];
+        final Object attr = topFrame.getLocalAttr(localVar.getSlotIndex());
+        if(attr != null) {
+          if (i != 0) {
+            pcBuilder.append("&&\\n");
+          }
+          pcBuilder.append(localVar.getName());
+          pcBuilder.append("=");
+          pcBuilder.append(attr.toString());
+        }
+      }
+      if(before != pcBuilder.length()){
+        pcBuilder.append("&&\\n");
+      }
+    }
+
+    if (pc != null) {
+      String[] pcs = pc.header.stringPC().split("&&");
+      for (int i = 0; i < pcs.length; i++) {
         pcBuilder.append(pcs[i]);
-        if(i != pcs.length - 1)
+        if (i != pcs.length - 1)
           pcBuilder.append(" &&\\n");
       }
-      return pcBuilder.append("\r").toString();
-    } else
-      return "";
+    }
+    return pcBuilder.append("\r").toString();
   }
 }
